6507b6aa8ab58a635d29927446d411825ffd3d3e
边城道馆 <248866053@qq.com>
2017-04-10 14:50:17 +0800

    yes, there is UTF-8

--- src/Lib.hs
+++ src/Lib.hs
@@ -14,6 +14,13 @@ module Lib
 
 import Control.Lens
 import Network.Wreq
+import Data.Aeson
+import Data.Aeson.Lens
+import Data.Map as Map
+import Data.Time.Clock
+
+import qualified Data.Text.Lazy.IO as L
+import Data.Text.Lazy.Encoding
 import qualified Network.Wreq.Session as S
 
 someFunc :: IO ()
@@ -24,3 +31,13 @@ someFunc = S.withSession $ \sess -> do
   -- Second request: the cookie should still be set afterwards.
   r <- S.post sess "http://httpbin.org/post" ["a" := (3 :: Int)]
   print $ r ^. responseCookie "name" . cookieValue
+
+
+loginCibhz :: IO ()
+loginCibhz = S.withSession $ \session -> do
+  -- S.get session "http://168.35.6.12:9999/zjcredit/login"
+  r <- S.post session "http://168.35.6.12:9999/zjcredit/login" ["loginname" := ("cibhzgl2" :: String), "password" := ( "abcd1234" ::String)]
+  print $ r
+  r1 <- S.post session "http://168.35.6.12:9999/zjcredit/enterprise-search" ["q" := ("杭州" :: String)]
+  L.putStr $ decodeUtf8 $ r1 ^. responseBody
+    -- ^. responseCookie "name" . cookieValue
