ddf9f15f7ad4feb1ad0abd49a64304f2a75c6b4f
边城道馆 <248866053@qq.com>
2017-04-11 16:57:44 +0800

    xml-conduit

--- src/Lib.hs
+++ src/Lib.hs
@@ -22,11 +22,47 @@ import qualified Data.Text.IO as Text
 import qualified Data.Text as Text
 
 import qualified Data.Text.Lazy.IO as L
+import qualified Data.Text.Lazy as Lazy
 import Data.Text.Lazy.Encoding
 import qualified Network.Wreq.Session as S
 import Text.HTML.DOM
 import Text.XML.Cursor (Cursor, attributeIs, content, element, fromDocument, child,
                         ($//), (&|), (&//), (>=>))
+
+import           Data.ByteString.Builder
+import qualified Data.ByteString.Lazy as LazyB
+import           Data.Monoid
+import           Data.Foldable                        (foldMap)
+import           Data.List                            (intersperse)
+
+data Cell = StringC String
+          | IntC Int
+          deriving( Eq, Ord, Show )
+
+type Row   = [Cell]
+type Table = [Row]
+
+encodeUtf8CSV :: Table -> LazyB.ByteString
+encodeUtf8CSV = toLazyByteString . renderTable
+
+renderTable :: Table -> Builder
+renderTable rs = mconcat [renderRow r <> charUtf8 '\n' | r <- rs]
+
+renderRow :: Row -> Builder
+renderRow []     = mempty
+renderRow (c:cs) =
+    renderCell c <> mconcat [ charUtf8 ',' <> renderCell c' | c' <- cs ]
+
+renderCell :: Cell -> Builder
+renderCell (StringC cs) = renderString cs
+renderCell (IntC i)     = intDec i
+
+renderString :: String -> Builder
+renderString cs = charUtf8 '"' <> foldMap escape cs <> charUtf8 '"'
+  where
+    escape '\\' = charUtf8 '\\' <> charUtf8 '\\'
+    escape '\"' = charUtf8 '\\' <> charUtf8 '\"'
+    escape c    = charUtf8 c
 
 someFunc :: IO ()
 someFunc = S.withSession $ \sess -> do
@@ -62,6 +98,18 @@ resPage url =
 
 page = L.putStr =<< resPage ""
 
+googlePage =
+  -- get "http://168.35.6.12:9999/zjcredit/login"
+  get "http://localhost:8000"
+  >>= return . (^. responseBody)
+
+axeses =
+  (Text.XML.Cursor.element "html" &// Text.XML.Cursor.element "h3")
+  
+parseGoogle =
+  googlePage >>=
+    return . axeses . fromDocument . parseLBS
+
 -- parse HTML
 
 -- Text.e URL we're going to search
@@ -69,14 +117,14 @@ url = "http://www.bing.com/search?q=school+of+haskell"
 
 -- Text.e data we're going to search for
 findNodes :: Cursor -> [Cursor]
-findNodes = Text.XML.Cursor.element "div"
+findNodes = Text.XML.Cursor.element "h3"
   -- >=> attributeIs "id" "count" >=> child
 
 -- Extract the data from each node in turn
 extractData = Text.concat . content
 
 -- Process the list of data elements
-processData =  putStrLn . Text.unpack . Text.concat
+processData =  Text.putStr . Text.concat
 
 cursorFor :: String -> IO Cursor
 cursorFor u = do
